e9295fb1d44ac729b59e08f7cc96cd87c0181082
Sergey Rozhkov <borzsonite@gmail.com>
2021-06-10 12:59:52 +0300

    lesson19 start

--- src/main/java/ru/alishev/springcourse/controller/SecondController.java
+++ src/main/java/ru/alishev/springcourse/controller/SecondController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
+public class SecondController {
+    @GetMapping("/exit")
+    public String exit() {
 
-    @GetMapping("/hello-world")
-    public String helloWorld() {
-
-        return "hello_world";
+        return "/second/exit";
     }
 }
